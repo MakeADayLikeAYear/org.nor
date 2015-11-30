fd7f34e36a2e4e554a3a68c61fce8eaaee8d876a
SSH <gamiwar@naver.com>
2015-11-30 09:39:44 +0900

    프로젝트 생성.

---
+++ org.nor.swt/src/main/java/org/nor/swt/SWTMain.java
@@ -0,0 +1,27 @@
+package org.nor.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class SWTMain {
+
+  public static void main(String[] args) {
+      Display display = new Display();
+      Shell shell = new Shell(display);
+      Label label = new Label(shell, SWT.NONE);
+    label.setText("Hello World!");
+    
+    shell.pack();
+    label.pack();
+      shell.open();
+      while(!shell.isDisposed())
+          if(!display.readAndDispatch())
+              display.sleep();
+          
+      display.dispose();
+      label.dispose();
+  }
+
+}
